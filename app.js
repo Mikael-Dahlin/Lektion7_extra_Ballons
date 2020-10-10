905e48b1299a5f317465968d9f2dceb7d5453f9c
Mikael-Dahlin <71758203+Mikael-Dahlin@users.noreply.github.com>
2020-10-10 14:08:46 +0200

    Pop the balloons
    hover over balloons to pop them

---
+++ app.js
@@ -0,0 +1,35 @@
+// declare variables from html elements
+const balloons = document.querySelectorAll(".button");
+const resetButton = document.querySelector("#reset");
+let alerted = false;
+
+// add event listener to balloon elements
+balloons.forEach((element) => {
+  element.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    pop(event.target);
+  });
+});
+
+// add event listener to Reset Balloons button
+resetButton.addEventListener("click", () => {
+  document.querySelectorAll(".popped").forEach((element) => {
+    element.textContent = "";
+    element.classList.remove("popped");
+    alerted = true;
+  });
+});
+
+// function to pop the balloon
+function pop(element) {
+  console.log(element);
+  element.textContent = "POP!";
+  element.classList.add("popped");
+  if (
+    balloons.length === document.querySelectorAll(".popped").length &&
+    !alerted
+  ) {
+    alerted = true;
+    alert("Congratulations you have popped all the balloons!");
+  }
+}
